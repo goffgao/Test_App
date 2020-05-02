46760fc20429bd8766557e633bfaf5df382352e4
qq102 <goffgao@163.com>
2020-05-02 22:18:41 +0800

    test

--- scripts/test_001.py
+++ scripts/test_001.py
@@ -4,14 +4,11 @@ import os
 import sys
 sys.path.append(os.path.split(os.path.dirname(os.path.abspath(__file__)))[0])
 
-import pytest , allure
+import pytest,allure
 
 class Test_Abc_001():
         @allure.step(title='第一个测试')
         def test_one(self):
             allure.attach('这是一个藐视','试一下')
             assert 1
-
-if __name__=="__main__":
-    pytest.main('-s --alluredir report')
 
